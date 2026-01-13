859b76a75c0787bd4ee415b703fafbae04d64207
xxxx <xxxx@gmail.com>
2026-01-13 15:54:16 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <div className=''>
+        <h1 className='text-red-500'>
+          salom
+        </h1>
+      </div>
+    </div>
+  )
+}
