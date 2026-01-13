68c750c1b386b9b59d3102350913aca81a96becf
xxxx <xxxx@gmail.com>
2026-01-13 16:22:41 +0500

    iconInstal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { AlignVerticalJustifyCenter } from 'lucide-react';
 export default function App() {
   return (
     <div>
       <div className=''>
         <h1 className='text-red-500'>
           salom
+           <AlignVerticalJustifyCenter />
         </h1>
       </div>
     </div>
