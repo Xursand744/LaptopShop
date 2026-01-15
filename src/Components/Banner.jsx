9b77e8bdc6f23fa6d17227d181be71f83d31f201
Guli <tojiboyevaguli3@gmail.com>
2026-01-15 16:12:32 +0500

    swiper

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,17 +1,23 @@
-
-      import React from 'react'
-
+import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/scrollbar';
-import burger from '../assets/burger.jpg'
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import laptop from '../assets/Laptop.jpg'
+import klaviatura from '../assets/Klaviatura.png'
+import klaviatura2 from '../assets/Klavoatura2.png'
+import mouse from '../assets/Mouse.png'
+import chair from '../assets/Chair.png'
+import car from '../assets/Car.png'
+import virtual from '../assets/Virtual.png'
+import football from '../assets/Football.jpg'
+import klaviatura3 from '../assets/Klaviatura3.png'
 
 
-// import required modules
-import { Scrollbar } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import { Grid2x2, PlayCircle } from "lucide-react";
 import { Settings } from "lucide-react";
 import { MonitorSpeaker } from "lucide-react";
@@ -43,28 +49,34 @@ export default function App() {
                 <p className=' underline underline-offset-4 decoration-pink-500'>Все вренды</p>
               </div>
               <div>
-              <Swiper
 
-        scrollbar={{
-          hide: true,
-        }}
-        
-        modules={[Scrollbar]}
-        className="mySwiper"
-      >
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-        <SwiperSlide><img src={burger} alt="" /></SwiperSlide>
-      </Swiper>
               </div>
             </div>
           </div>
+          <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img className="w-full h-full object-cover" src={laptop} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="w-full h-full object-cover" src={klaviatura} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="w-full h-full object-cover" src={klaviatura2} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="w-full h-full object-cover" src={mouse} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="w-full h-full object-cover" src={chair} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="w-full h-full object-cover" src={car} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="w-full h-full object-cover" src={virtual} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="w-full h-full object-cover" src={football} alt="" /></SwiperSlide>
+        <SwiperSlide><img className="w-full h-full object-cover" src={klaviatura3} alt="" /></SwiperSlide>
+      </Swiper>
         </header>
       </div>
     </div>
