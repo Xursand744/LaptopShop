2497824b5fe285358c775acb229f20909c3d397d
watch <asd@gmail.com>
2026-01-17 16:23:09 +0300

    kon..

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 import Banner from './Components/Banner.jsx'
+import Konfigurator from './Components/Konfigurator.jsx'
+import Navbar from './Components/Navbar.jsx'
+import Acsesuars from './Components/Acsesuars.jsx'
 export default function App() {
   return (
     <div>
       <Banner/>
+      <Konfigurator/>
+      <Navbar/>
+      <Acsesuars/>
     </div>
   )
 }
